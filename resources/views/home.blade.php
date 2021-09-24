7bbcd4fefd29661d434472a3a3b4db8445b2c6fe
WesleyHaring <D284479@edu.rocwb.nl>
2021-09-24 10:16:42 +0200

    zwerkbal applicatie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,24 @@
 
 @section('content')
 
-    <h1>Test</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
+    <h1>Dashboard</h1>
+    <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van Joanne Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam. Het idee van Zwerkbal lijkt te zijn gebaseerd op het non-fictieve horseball.</p>
+    <div class="grid">
+        <div class="item border">
+            <p>Aantal aankomende tournooien:</p>
+            <span> 5 </span>
+        </div>
 
+        <div class="item-large border">
+         <img src="../../img/snitch.png" alt="">
+        </div>
+
+        <div class="item border">
+            <p>Totaal aantal teams:</p>   
+            <span> 26 </span>     
+        </div>
+
+
+    </div>
+    
 @endsection
