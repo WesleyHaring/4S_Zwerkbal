e255501b84fdc42d9dae07068fb188ed5c2c9464
WesleyHaring <D284479@edu.rocwb.nl>
2021-10-08 10:35:47 +0200

    tournament edit wrekt

--- resources/views/tournaments/create.blade.php
+++ resources/views/tournaments/create.blade.php
@@ -16,7 +16,16 @@
 		<div class="form-group">
 			<label for="name">Naam toernooi</label>
 			<input type="text" id="name" name="name" class="form-control">
+			<input type="date" id="date" name="date" class="form-control">
+			<input type="time" id="start_time" name="start_time" class="form-control">
 		</div>
 		<button type="submit">Opslaan</button>
 	</form>
 @endsection
+@if($errors->any())
+	<ul class="errors">
+		@foreach($errors->all() as $error)
+			<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+@endif
